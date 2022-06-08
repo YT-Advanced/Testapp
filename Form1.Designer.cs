da8a9429b593fd1388c2db652b4b8ca0d646b628
YT Advanced <70064328+YT-Advanced@users.noreply.github.com>
2022-06-08 11:09:59 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Khôi_phục_JPEG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.InputTab = new System.Windows.Forms.TabPage();
+            this.OutputTab = new System.Windows.Forms.TabPage();
+            this.LogBox = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.InputTab.SuspendLayout();
+            this.OutputTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(698, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder with encrypted files:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(19, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(673, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Location = new System.Drawing.Point(19, 332);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(673, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Repaired folder";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(698, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Choose your file to repair:";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(19, 116);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(772, 165);
+            this.checkedListBox1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(342, 366);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 48);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.InputTab);
+            this.tabControl1.Controls.Add(this.OutputTab);
+            this.tabControl1.Location = new System.Drawing.Point(-2, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(802, 454);
+            this.tabControl1.TabIndex = 15;
+            // 
+            // InputTab
+            // 
+            this.InputTab.Controls.Add(this.checkedListBox1);
+            this.InputTab.Controls.Add(this.button2);
+            this.InputTab.Controls.Add(this.button3);
+            this.InputTab.Controls.Add(this.button1);
+            this.InputTab.Controls.Add(this.label1);
+            this.InputTab.Controls.Add(this.textBox1);
+            this.InputTab.Controls.Add(this.label3);
+            this.InputTab.Controls.Add(this.label2);
+            this.InputTab.Controls.Add(this.textBox2);
+            this.InputTab.Location = new System.Drawing.Point(4, 29);
+            this.InputTab.Name = "InputTab";
+            this.InputTab.Padding = new System.Windows.Forms.Padding(3);
+            this.InputTab.Size = new System.Drawing.Size(794, 421);
+            this.InputTab.TabIndex = 0;
+            this.InputTab.Text = "Input";
+            this.InputTab.UseVisualStyleBackColor = true;
+            // 
+            // OutputTab
+            // 
+            this.OutputTab.Controls.Add(this.LogBox);
+            this.OutputTab.Location = new System.Drawing.Point(4, 29);
+            this.OutputTab.Name = "OutputTab";
+            this.OutputTab.Padding = new System.Windows.Forms.Padding(3);
+            this.OutputTab.Size = new System.Drawing.Size(794, 417);
+            this.OutputTab.TabIndex = 1;
+            this.OutputTab.Text = "Output";
+            this.OutputTab.UseVisualStyleBackColor = true;
+            // 
+            // LogBox
+            // 
+            this.LogBox.Location = new System.Drawing.Point(7, 16);
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(776, 161);
+            this.LogBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.InputTab.ResumeLayout(false);
+            this.InputTab.PerformLayout();
+            this.OutputTab.ResumeLayout(false);
+            this.OutputTab.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage InputTab;
+        private System.Windows.Forms.TabPage OutputTab;
+        private System.Windows.Forms.TextBox LogBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using OSGeo.GDAL;
+
+namespace Khôi_phục_JPEG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox1.Text = fbd.SelectedPath;
+                    textBox2.Text = fbd.SelectedPath + "\\repaired";
+                    string[] filePaths = Directory.GetFiles(fbd.SelectedPath, @"*.*");
+                    checkedListBox1.Items.Clear();
+                    int i = 0;
+                    foreach (string filePath in filePaths)
+                    {
+                        checkedListBox1.Items.Add(filePath.Replace(textBox1.Text + "\\",""));
+                        checkedListBox1.SetItemChecked(i, true);
+                        i++;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox2.Text = fbd.SelectedPath;
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(OutputTab);
+            Directory.CreateDirectory(textBox2.Text);
+            foreach (string file in checkedListBox1.CheckedItems)
+            {
+                byte[] data = File.ReadAllBytes(textBox1.Text + "\\" + file).Skip(153605).ToArray();
+                File.WriteAllBytes(textBox2.Text + "\\" + file, data);
+                Process p = new Process();
+                p.StartInfo.FileName = "JpegRecovery.exe";
+                p.StartInfo.Arguments = textBox2.Text + "\\" + file;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.Start();
+                p.WaitForExit();
+                File.Delete(textBox2.Text + "\\" + file);
+                new WhiteBalance().AplyWhiteBalance(textBox2.Text + "\\" + file + ".jpg", textBox2.Text + "\\" + file.Substring(0, file.IndexOf(".")) + ".tiff");
+                File.Delete(textBox2.Text + "\\" + file + ".jpg");
+                System.Drawing.Bitmap.FromFile(textBox2.Text + "\\" + file.Substring(0, file.IndexOf(".")) + ".tiff").Save(textBox2.Text + "\\" + file.Substring(0, file.IndexOf(".")) + ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                LogBox.Text = LogBox.Text + file + " - REPAIRED\r\n";
+            }
+        }
+        public class WhiteBalance
+        {
+
+            private double percentForBalance = 0.6;
+
+            public WhiteBalance()
+            {
+                GdalConfiguration.ConfigureGdal();
+            }
+
+            public WhiteBalance(double percentForBalance)
+            {
+                this.percentForBalance = percentForBalance;
+                GdalConfiguration.ConfigureGdal();
+            }
+
+            public void AplyWhiteBalance(string imagePath, string outImagePath)
+            {
+
+                using (Dataset image = Gdal.Open(imagePath, Access.GA_ReadOnly))
+                {
+
+                    Band redBand = GetBand(image, ColorInterp.GCI_RedBand);
+                    Band greenBand = GetBand(image, ColorInterp.GCI_GreenBand);
+                    Band blueBand = GetBand(image, ColorInterp.GCI_BlueBand);
+
+                    if (redBand == null || greenBand == null || blueBand == null)
+                    {
+                        throw new NullReferenceException("One or more bands are not available.");
+                    }
+
+                    int width = redBand.XSize;
+                    int height = redBand.YSize;
+
+                    using (Dataset outImage = Gdal.GetDriverByName("GTiff").Create(outImagePath, width, height, 3, DataType.GDT_Byte, null))
+                    {
+
+                        double[] geoTransformerData = new double[6];
+                        image.GetGeoTransform(geoTransformerData);
+                        outImage.SetGeoTransform(geoTransformerData);
+                        outImage.SetProjection(image.GetProjection());
+
+                        Band outRedBand = outImage.GetRasterBand(1);
+                        Band outGreenBand = outImage.GetRasterBand(2);
+                        Band outBlueBand = outImage.GetRasterBand(3);
+
+                        int[] red = new int[width * height];
+                        int[] green = new int[width * height];
+                        int[] blue = new int[width * height];
+                        redBand.ReadRaster(0, 0, width, height, red, width, height, 0, 0);
+                        greenBand.ReadRaster(0, 0, width, height, green, width, height, 0, 0);
+                        blueBand.ReadRaster(0, 0, width, height, blue, width, height, 0, 0);
+
+                        int[] outRed = WhiteBalanceBand(red);
+                        int[] outGreen = WhiteBalanceBand(green);
+                        int[] outBlue = WhiteBalanceBand(blue);
+                        outRedBand.WriteRaster(0, 0, width, height, outRed, width, height, 0, 0);
+                        outGreenBand.WriteRaster(0, 0, width, height, outGreen, width, height, 0, 0);
+                        outBlueBand.WriteRaster(0, 0, width, height, outBlue, width, height, 0, 0);
+
+                        outImage.FlushCache();
+                    }
+                }
+            }
+
+            public int[] WhiteBalanceBand(int[] band)
+            {
+                int[] sortedBand = new int[band.Length];
+                Array.Copy(band, sortedBand, band.Length);
+                Array.Sort(sortedBand);
+
+                double perc05 = Percentile(sortedBand, percentForBalance);
+                double perc95 = Percentile(sortedBand, 100.0 - percentForBalance);
+
+                int[] bandBalanced = new int[band.Length];
+
+                for (int i = 0; i < band.Length; i++)
+                {
+
+                    double valueBalanced = (band[i] - perc05) * 255.0 / (perc95 - perc05);
+                    bandBalanced[i] = LimitToByte(valueBalanced);
+                }
+
+                return bandBalanced;
+            }
+
+            public double Percentile(int[] sequence, double percentile)
+            {
+
+                int nSequence = sequence.Length;
+                double nPercent = (nSequence + 1) * percentile / 100d;
+                if (nPercent == 1d)
+                {
+                    return sequence[0];
+                }
+                else if (nPercent == nSequence)
+                {
+                    return sequence[nSequence - 1];
+                }
+                else
+                {
+                    int intNPercent = (int)nPercent;
+                    double d = nPercent - intNPercent;
+                    return sequence[intNPercent - 1] + d * (sequence[intNPercent] - sequence[intNPercent - 1]);
+                }
+            }
+
+            private byte LimitToByte(double value)
+            {
+                byte newValue;
+
+                if (value < 0)
+                {
+                    newValue = 0;
+                }
+                else if (value > 255)
+                {
+                    newValue = 255;
+                }
+                else
+                {
+                    newValue = (byte)value;
+                }
+
+                return newValue;
+            }
+
+            /**
+              * Returns the band for an color (red, green, blue or alpha)
+              * The dataset should have 4 bands
+              * */
+            public static Band GetBand(Dataset ImageDataSet, ColorInterp colorInterp)
+            {
+                if (colorInterp.Equals(ImageDataSet.GetRasterBand(1).GetRasterColorInterpretation()))
+                {
+                    return ImageDataSet.GetRasterBand(1);
+                }
+                else if (colorInterp.Equals(ImageDataSet.GetRasterBand(2).GetRasterColorInterpretation()))
+                {
+                    return ImageDataSet.GetRasterBand(2);
+                }
+                else if (colorInterp.Equals(ImageDataSet.GetRasterBand(3).GetRasterColorInterpretation()))
+                {
+                    return ImageDataSet.GetRasterBand(3);
+                }
+                else
+                {
+                    return ImageDataSet.GetRasterBand(4);
+                }
+            }
+
+        }
+    }
+}
